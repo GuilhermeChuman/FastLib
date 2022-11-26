0b5acb0576f35baf93696e5bb7826a38a3b7e429
GuilhermeChuman <guilhermectroncon@gmail.com>
2022-11-25 21:18:06 -0300

    recuperacao de senha

--- env.py
+++ env.py
@@ -10,8 +10,8 @@ class Mail:
         
         return {
             #local configs
-            'sender_email': 'atendimento@silverfishlib.com.br',
-            'password': 'Newsenha+4',
+            'sender_email': '',
+            'password': '',
             'smtp_server': 'email-ssl.com.br',
             'port': 465 
 
